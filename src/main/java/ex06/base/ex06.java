883a6b9b5904c58c104fed684beb149c5229d91a
rachelsch1 <schwarz.rachel.e@gmail.com>
2021-06-06 12:10:42 -0400

    initial solution

--- src/main/java/ex06/base/ex06.java
+++ src/main/java/ex06/base/ex06.java
@@ -1,4 +1,28 @@
 package ex06.base;
 
+import java.util.Calendar;
+import java.util.Scanner;
+
 public class ex06 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is your current age? ");
+        String str1 = input.nextLine();
+        int currAge = Integer.parseInt(str1);
+
+        System.out.print("At what age would you like to retire? ");
+        String str2 = input.nextLine();
+        int retireAge = Integer.parseInt(str2);
+
+        int yrLeft = retireAge - currAge;
+        int currYear = Calendar.getInstance().get(Calendar.YEAR);
+        int retireYear = currYear + yrLeft;
+
+        String outputString1 = "You have " + yrLeft + " years left until you can retire.";
+        String outputString2 = "It's " + currYear + " so you can retire in " + retireYear + ".";
+
+        System.out.println(outputString1);
+        System.out.println(outputString2);
+    }
 }
